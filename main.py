9c154d53990d74d6e378b0eb656feaf8032e2d8e
S2FFY <avengerhotblitz@gmail.com>
2025-10-28 03:53:23 +0300

    First app

--- main.py
+++ main.py
@@ -1,6 +1,33 @@
+import flet as ft
 
+def main(page: ft.Page):
+    page.title = "My Tasks"
+    page.theme_mode = "dark"
 
-print('Test')
+    navigation = ft.Column(
+        controls=[
+            ft.IconButton(ft.Icons.HOME),
+            ft.IconButton(ft.Icons.SETTINGS)
+        ],
+        alignment=ft.MainAxisAlignment.START,
+        spacing=0
+    )
 
-x = 5
-print(x)
+    sidebar = ft.Container(
+        content=navigation,
+        width=50,
+        padding=0,
+        bgcolor='#000000',
+        alignment=ft.alignment.top_left,
+
+    )
+    page.padding = 0
+
+    main_area = ft.Container(
+        content=ft.Text('Здесь область работы'),
+        padding=20
+    )
+
+    page.add(ft.Row([sidebar, main_area], expand=True))
+
+ft.app(target=main)
