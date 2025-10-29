197a2df8246bb1e3870bbfc85c991dedb01e2180
S2FFY <avengerhotblitz@gmail.com>
2025-10-29 23:18:06 +0300

    fix switch_theme

--- main.py
+++ main.py
@@ -2,32 +2,43 @@ import flet as ft
 
 def main(page: ft.Page):
     page.title = "My Tasks"
-    page.theme_mode = "dark"
+    page.theme_mode = ft.ThemeMode.DARK
 
-    navigation = ft.Column(
+    def switch_theme(e):
+        if page.theme_mode == ft.ThemeMode.DARK:
+            page.theme_mode = ft.ThemeMode.LIGHT
+            sidebar.bgcolor = '#ffffff'
+        else:
+            page.theme_mode = ft.ThemeMode.DARK
+            sidebar.bgcolor = '#000000'
+        page.update()
+
+    nav = ft.Column(
         controls=[
-            ft.IconButton(ft.Icons.HOME),
-            ft.IconButton(ft.Icons.SETTINGS)
+            ft.IconButton(ft.Icons.TASK_ALT),
+            ft.IconButton(ft.Icons.LOOP),
+            ft.IconButton(ft.Icons.GRID_VIEW),
+            ft.IconButton(ft.Icons.TIMER),
+            ft.IconButton(ft.Icons.SUNNY, on_click=switch_theme),
         ],
         alignment=ft.MainAxisAlignment.START,
         spacing=0
     )
 
     sidebar = ft.Container(
-        content=navigation,
-        width=50,
+        content=nav,
+        width=40,
         padding=0,
         bgcolor='#000000',
-        alignment=ft.alignment.top_left,
-
+        alignment=ft.alignment.top_left
     )
-    page.padding = 0
 
     main_area = ft.Container(
-        content=ft.Text('Здесь область работы'),
-        padding=20
+        content=ft.Text('Kjk'),
+        padding=20,
+        expand=True
     )
-
+    page.padding=0
     page.add(ft.Row([sidebar, main_area], expand=True))
 
 ft.app(target=main)
